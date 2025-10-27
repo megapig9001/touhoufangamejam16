eaec138239bbbdadc64082aecebf2f474bdddbb9
shrooby <61812434+shroobert@users.noreply.github.com>
2025-10-26 21:03:29 -0700

    Make cloth item disappear on level clear

--- Assets/Scripts/Objects/LevelGoal.cs
+++ Assets/Scripts/Objects/LevelGoal.cs
@@ -1,16 +1,45 @@
+using DG.Tweening;
+using EventManager;
+using System;
 using UnityEngine;
 
 public class LevelGoal : MonoBehaviour
 {
+    [SerializeField]
+    GameObject goalItem;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PlayerHead"))
         {
             JSAM.AudioManager.PlaySound(AudioLibrarySounds.Clear);
-            var goalEvent = new EventManager.PlayerReachGoalEvent();
+            var goalEvent = new PlayerReachGoalEvent();
             goalEvent.goalPosition = transform.position;            
             goalEvent.InvokeEvent();
+
+            if(goalItem != null)
+            {
+                goalItem.transform.DOScale(0, 1);
+            }
         }
 
     }
+
+    private void OnEnable()
+    {
+        LevelRestartEvent.AddListener(HandlePlayerDeathEvent);
+
+    }
+
+
+    private void OnDisable()
+    {
+        LevelRestartEvent.RemoveListener(HandlePlayerDeathEvent);
+    }
+
+    private void HandlePlayerDeathEvent(LevelRestartEvent info)
+    {
+        goalItem.transform.localScale = Vector3.one;
+    }
+
 }
